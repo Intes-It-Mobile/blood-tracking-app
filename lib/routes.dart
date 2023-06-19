9ed8fbf00800de96417f10d4a227201134311755
Kieu Thang <kieuthang217@gmail.com>
2023-06-19 17:04:01 +0700

    fix EditRange

--- lib/routes.dart
+++ lib/routes.dart
@@ -2,6 +2,7 @@ import 'package:blood_sugar_tracking/views/edit_range/edit_range_screens.dart';
 import 'package:blood_sugar_tracking/views/home/home_screen.dart';
 import 'package:blood_sugar_tracking/views/infomation/information_detail_screen.dart';
 import 'package:blood_sugar_tracking/views/new_record/new_record_screen.dart';
+import 'package:blood_sugar_tracking/views/record_remind/record_remind_screens.dart';
 import 'package:blood_sugar_tracking/views/setting/abt_us_screen.dart';
 import 'package:blood_sugar_tracking/views/splash/splash_screen.dart';
 import 'package:flutter/cupertino.dart';
@@ -17,6 +18,7 @@ class Routes {
   static const String abt_us = '/abt_us';
   static const String new_record = '/new_record';
   static const String edit_range = '/edit_range';
+  static const String record_remind = '/record_remind';
 
   static final routes = <String, WidgetBuilder>{
     splash: (context) => SplashScreen(),
@@ -25,6 +27,7 @@ class Routes {
     abt_us: (context) => AboutUsScreen(),
     new_record: (context) => NewRecordScreen(),
     edit_range: (context) => EditRangeScreens(),
+    record_remind: (context) => RecordRemindScreens(),
   };
 
   static Route<dynamic>? onGenerateRoute(RouteSettings settings) {
