558d57ce3171760dabf1d09bd62b96b28cc76f50
Kieu Thang <kieuthang217@gmail.com>
2023-07-03 14:40:36 +0700

    fix UI setting

--- lib/utils/dialog/dialog_feedback.dart
+++ lib/utils/dialog/dialog_feedback.dart
@@ -17,6 +17,8 @@ class DialogFeedback extends StatefulWidget {
 }
 
 class _DialogFeedbackState extends State<DialogFeedback> {
+  final formKey = GlobalKey<FormState>();
+  bool isSelected = false;
 
   @override
   Widget build(BuildContext context) {
@@ -60,88 +62,117 @@ class _DialogFeedbackState extends State<DialogFeedback> {
         ],
       ),
       content: SingleChildScrollView(
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
-            child: Container(
-              child: Text(
-                "${AppLocalizations.of(context)!.getTranslate('email_if_have')}",
-                style: AppTheme.appBodyTextStyle
-                    .copyWith(color: AppColors.AppColor4),
-              ),
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 20),
-            decoration: BoxDecoration(
-              border: Border.all(color: AppColors.AppColor4),
-              borderRadius: const BorderRadius.all(
-                Radius.circular(10),
-              ),
-            ),
-            child: TextFormField(
-              decoration: InputDecoration(
-                  hintText:
-                      "${AppLocalizations.of(context)!.getTranslate('email_label_txt')}",
-                  hintStyle: AppTheme.hintText,
-                  border: InputBorder.none,
-                  contentPadding: EdgeInsets.fromLTRB(10, 10, 10, 13)),
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 12, horizontal: 20),
-            child: Container(
-              child: Text(
-                "${AppLocalizations.of(context)!.getTranslate('feedback_write_dwn')}",
-                style: AppTheme.appBodyTextStyle
-                    .copyWith(color: AppColors.AppColor4),
-              ),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 20),
-            decoration: BoxDecoration(
-              border: Border.all(color: AppColors.AppColor4),
-              borderRadius: BorderRadius.all(
-                Radius.circular(10),
-              ),
-            ),
-            child: TextFormField(
-              maxLines: 4,
-              decoration: InputDecoration(
-                  hintText:
-                      "${AppLocalizations.of(context)!.getTranslate('feedback_write_dwn_label_txt')}",
-                  hintStyle: AppTheme.hintText,
-                  border: InputBorder.none,
-                  contentPadding: EdgeInsets.fromLTRB(10, 10, 10, 20)),
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 12, horizontal: 20),
-            child: Center(
-              child: Text(
-                "${AppLocalizations.of(context)!.getTranslate('feedback_help')}",
-                style: AppTheme.appBodyTextStyle
-                    .copyWith(color: AppColors.AppColor4),
-              ),
-            ),
-          ),
-          Center(
-            child: Container(
-              decoration: BoxDecoration(
-                color: AppColors.AppColor2,
-                borderRadius: BorderRadius.all(
-                  Radius.circular(5),
+        child: Form(
+          key: formKey,
+          child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+              child: Container(
+                child: Text(
+                  "${AppLocalizations.of(context)!.getTranslate('email_if_have')}",
+                  style: AppTheme.appBodyTextStyle
+                      .copyWith(color: AppColors.AppColor4),
                 ),
               ),
-              padding: EdgeInsets.symmetric(horizontal: 13, vertical: 10),
-              child: Text(
-                "${AppLocalizations.of(context)!.getTranslate('feedback_send_btn')}",
-                style: AppTheme.BtnText,
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                border: Border.all(color: AppColors.AppColor4),
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(10),
+                ),
+              ),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    hintText:
+                        "${AppLocalizations.of(context)!.getTranslate('email_label_txt')}",
+                    hintStyle: AppTheme.hintText,
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.fromLTRB(10, 10, 10, 13)),
               ),
             ),
-          )
-        ]),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+              child: Container(
+                child: Text(
+                  "${AppLocalizations.of(context)!.getTranslate('feedback_write_dwn')}",
+                  style: AppTheme.appBodyTextStyle
+                      .copyWith(color: AppColors.AppColor4),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                border: Border.all(color: AppColors.AppColor4),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10),
+                ),
+              ),
+              child: TextFormField(
+                maxLines: 4,
+                decoration: InputDecoration(
+                    hintText:
+                        "${AppLocalizations.of(context)!.getTranslate('feedback_write_dwn_label_txt')}",
+                    hintStyle: AppTheme.hintText,
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.fromLTRB(10, 10, 10, 20)),
+                onChanged: (value){
+                  if(value.length >= 6){
+                    setState(() {
+                      isSelected = true;
+                    });
+                  }else{
+                    setState(() {
+                      isSelected = false;
+                    });
+                  }
+                },
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+              child: Center(
+                child: Text(
+                  "${AppLocalizations.of(context)!.getTranslate('feedback_help')}",
+                  style: AppTheme.appBodyTextStyle
+                      .copyWith(color: AppColors.AppColor4),
+                ),
+              ),
+            ),
+            Center(
+              child: isSelected == false ? Opacity(
+                opacity: 0.3,
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: AppColors.AppColor2,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(5),
+                    ),
+                  ),
+                  padding: const EdgeInsets.symmetric(horizontal: 13, vertical: 10),
+                  child: Text(
+                    "${AppLocalizations.of(context)!.getTranslate('feedback_send_btn')}",
+                    style: AppTheme.BtnText,
+                  ),
+                ),
+              ) : Container(
+                decoration: const BoxDecoration(
+                  color: AppColors.AppColor2,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(5),
+                  ),
+                ),
+                padding: const EdgeInsets.symmetric(horizontal: 13, vertical: 10),
+                child: Text(
+                  "${AppLocalizations.of(context)!.getTranslate('feedback_send_btn')}",
+                  style: AppTheme.BtnText,
+                ),
+              ),
+            )
+          ]),
+        ),
       ),
       actions: [],
     );
