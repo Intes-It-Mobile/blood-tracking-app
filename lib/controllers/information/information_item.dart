854b09a3aca0938b0f0a5121b43d4a4fc0da6b51
unknown <kieuvietthang>
2023-08-03 17:40:46 +0700

    fix ui

--- lib/controllers/information/information_item.dart
+++ lib/controllers/information/information_item.dart
@@ -4,15 +4,9 @@ class ListInformation{
   List<Information> information = [
     Information(
       gender: 'Male',
-      weight: 20,
-      tall: 20,
-      old: 20
     ),
     Information(
       gender: 'Female',
-        weight: 20,
-        tall: 20,
-        old: 20
     ),
   ];
 }
