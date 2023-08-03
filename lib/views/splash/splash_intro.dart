d3240fbb04ead05363a65d33619b2d75206cb3b4
unknown <kieuvietthang>
2023-08-03 14:50:28 +0700

    fix ui intro

--- lib/views/splash/splash_intro.dart
+++ lib/views/splash/splash_intro.dart
@@ -39,126 +39,137 @@ class _IntroScreenState extends State<IntroScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.58,
-            child: PageView.builder(
-                itemCount: intro.length,
-                controller: _pageController,
-                physics: const BouncingScrollPhysics(),
-                onPageChanged: (int index) {
-                  setState(() {
-                    currentIndex = index;
-                    indexPage = index;
-                  });
-                },
-                itemBuilder: (_, index) {
-                  return buildPage(
-                    intro[index]["img"],
-                    intro[index]["text"],
-                  );
-                }),
-          ),
-          Container(
-            margin: EdgeInsets.only(bottom: 48, right: 16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              mainAxisAlignment: MainAxisAlignment.end,
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Column(
+          children: [
+            Stack(
               children: [
-                Container(
-                  margin: EdgeInsets.only(bottom: 28),
-                  alignment: Alignment.bottomRight,
-                  child: SmoothPageIndicator(
-                    controller: _pageController,
-                    count: 4,
-                    effect: SlideEffect(
-                        radius: 2,
-                        paintStyle: PaintingStyle.stroke,
-                        strokeWidth: 1,
-                        activeDotColor: AppColors.AppColor4,
-                        dotColor: AppColors.AppColor4,
-                        dotHeight: 8,
-                        dotWidth: 18),
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    indexPage == 3
-                        ? Container()
-                        : GestureDetector(
-                      onTap: () {
-                        shareLocal.putBools("isFirst", true);
-                        Navigator.pushNamedAndRemoveUntil(
-                          context,
-                          Routes.home,
-                              (route) => false,
-                        );
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.62,
+                  child: PageView.builder(
+                      itemCount: intro.length,
+                      controller: _pageController,
+                      physics: const BouncingScrollPhysics(),
+                      onPageChanged: (int index) {
+                        setState(() {
+                          currentIndex = index;
+                          indexPage = index;
+                        });
                       },
-                      child: Container(
-                        width: 120,
-                        height: 36,
-                        padding: EdgeInsets.symmetric(horizontal: 25, vertical: 6),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(5),
-                          border: Border.all(
-                            color: AppColors.AppColor2,
+                      itemBuilder: (_, index) {
+                        return buildPage(
+                          intro[index]["img"],
+                          intro[index]["text"],
+                        );
+                      }),
+                ),
+                Positioned(
+                  bottom: 0,
+                  right: 0,
+                  child: Container(
+                    margin: EdgeInsets.only(bottom: 5, right: 16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(bottom: 28),
+                          alignment: Alignment.bottomRight,
+                          // color: AppColors.AppColor3,
+                          child: SmoothPageIndicator(
+                            controller: _pageController,
+                            count: 4,
+                            effect: SlideEffect(
+                                radius: 2,
+                                paintStyle: PaintingStyle.fill,
+                                strokeWidth: 1,
+                                activeDotColor: Color(0xFF00FFFF),
+                                dotColor: AppColors.AppColor3,
+                                dotHeight: 8,
+                                dotWidth: 18),
                           ),
                         ),
-                        child: Text(
-                          "${AppLocalizations.of(context)!.getTranslate('skip_all')}",
-                          textAlign: TextAlign.center,
-                          style: AppTheme.TextIntroline16Text.copyWith(color: AppColors.AppColor2),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            indexPage == 3
+                                ? Container()
+                                : GestureDetector(
+                              onTap: () {
+                                shareLocal.putBools("isFirst", true);
+                                Navigator.pushNamedAndRemoveUntil(
+                                  context,
+                                  Routes.home,
+                                      (route) => false,
+                                );
+                              },
+                              child: Container(
+                                width: 120,
+                                height: 36,
+                                padding: EdgeInsets.symmetric(horizontal: 25, vertical: 6),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(5),
+                                  border: Border.all(
+                                    color: AppColors.AppColor2,
+                                  ),
+                                ),
+                                child: Text(
+                                  "${AppLocalizations.of(context)!.getTranslate('skip_all')}",
+                                  textAlign: TextAlign.center,
+                                  style: AppTheme.TextIntroline16Text.copyWith(color: AppColors.AppColor2),
+                                ),
+                              ),
+                            ),
+                            GestureDetector(
+                              onTap: () async {
+                                if (indexPage == 3) {
+                                  shareLocal.putBools("isFirst", true);
+                                  Navigator.pushNamedAndRemoveUntil(
+                                    context,
+                                    Routes.home,
+                                        (route) => false,
+                                  );
+                                }
+                                _pageController.nextPage(
+                                  duration: const Duration(milliseconds: 100),
+                                  curve: Curves.bounceIn,
+                                );
+                              },
+                              child: Container(
+                                width: 120,
+                                height: 36,
+                                margin: EdgeInsets.only(left: 17),
+                                padding: EdgeInsets.symmetric(horizontal: 2, vertical: 6),
+                                decoration: BoxDecoration(
+                                  gradient:
+                                  LinearGradient(begin: Alignment.topCenter, end: Alignment.bottomCenter, colors: [
+                                    AppColors.AppColor4,
+                                    AppColors.AppColor2,
+                                  ]),
+                                  borderRadius: BorderRadius.circular(5),
+                                ),
+                                child: Text(
+                                  indexPage == 3
+                                      ? "${AppLocalizations.of(context)!.getTranslate('homepage')}"
+                                      : "${AppLocalizations.of(context)!.getTranslate('next_step')}",
+                                  textAlign: TextAlign.center,
+                                  style: AppTheme.TextIntroline16Text.copyWith(fontWeight: FontWeight.w500),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
+                      ],
                     ),
-                    GestureDetector(
-                      onTap: () async {
-                        if (indexPage == 3) {
-                          shareLocal.putBools("isFirst", true);
-                          Navigator.pushNamedAndRemoveUntil(
-                            context,
-                            Routes.home,
-                                (route) => false,
-                          );
-                        }
-                        _pageController.nextPage(
-                          duration: const Duration(milliseconds: 100),
-                          curve: Curves.bounceIn,
-                        );
-                      },
-                      child: Container(
-                        width: 120,
-                        height: 36,
-                        margin: EdgeInsets.only(left: 17),
-                        padding: EdgeInsets.symmetric(horizontal: 2, vertical: 6),
-                        decoration: BoxDecoration(
-                          gradient:
-                          LinearGradient(begin: Alignment.topCenter, end: Alignment.bottomCenter, colors: [
-                            AppColors.AppColor4,
-                            AppColors.AppColor2,
-                          ]),
-                          borderRadius: BorderRadius.circular(5),
-                        ),
-                        child: Text(
-                          indexPage == 3
-                              ? "${AppLocalizations.of(context)!.getTranslate('homepage')}"
-                              : "${AppLocalizations.of(context)!.getTranslate('next_step')}",
-                          textAlign: TextAlign.center,
-                          style: AppTheme.TextIntroline16Text.copyWith(fontWeight: FontWeight.w500),
-                        ),
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
               ],
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
@@ -187,7 +198,7 @@ class _IntroScreenState extends State<IntroScreen> {
               alignment: Alignment.bottomLeft,
               child: Text(
                 "${AppLocalizations.of(context)!.getTranslate(textIntro)}",
-                style: AppTheme.TextIntroline16Text,
+                style: AppTheme.Intro20Text,
               )),
         ],
       ),
